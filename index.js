27094d715d787fc2c8cc4dae4218644b8c18af4e
AKASH D <126537594+akashtech17@users.noreply.github.com>
2023-09-30 20:58:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,153 @@
+
+                        // for loop statement 
+
+// for (let i =1;i<=10;i++){
+//     if(i%2 !==0){
+//         console.log(`oddnumber: ${i}`);
+//     }
+// }
+
+
+
+                        // while loop 
+
+// let r=1;
+// while(r<=20){
+//     if(r%2 !==0){
+//         console.log(`oddnumber in while loop ${r}`);
+//     }
+//     r++;
+// }
+
+
+
+
+                        // do while loop
+
+// let i=0;
+// do{
+//     if(i%2 !==0){
+//         console.log(`this is do while conduction ${i}`);
+//     }
+//     i++
+// } while(i<=10);                       
+
+
+
+
+
+                // for in loop 
+
+// const person = {
+//     name:"Rdx_Akash",
+//     age:23,
+//     sex:"Male"
+// }   ;        
+
+// for(let key in person){
+//     console.log(key +":" , person{key]);
+// }
+
+                // for of loop
+// let colors =["red","green","blue"];
+// for(let color of colors){
+//     console.log(`colors: ${colors}`);
+// }
+
+                        // oop object oriented programing
+
+// let person = {
+//     names:"akash",
+//     age:23,
+//     intrest:'books',
+//     isAlive:true,
+//     address : {
+//         city:"Vaniyambadi",
+//         state : "THIRUPATHURE"
+
+//     },
+//     gretting:function(){
+//         let msg= `My Name is ${this.names} and i'm From ${this.address.city}`;
+//         console.log(msg);
+//     }
+// };
+// person.gretting();
+
+            // factory functions
+
+// function createPerson(Name){
+//     return{
+//         Name: Name,
+//         gretting:function(){
+//             let msg=`my name is ${this.Name}`;
+//             console.log(msg);
+//         }
+//     };
+
+// }            
+
+// let anbu=createPerson('AKASH');
+// let rdx = createPerson('amos');
+
+// anbu.gretting();
+// rdx.gretting();
+
+            //constructor function
+
+// function asd(a) {
+//     this.name=a;
+//     this.greeting = function() {
+//         console.log(`My name is ${this.name}`);
+//     }
+
+// }
+// let person=new asd("Akash");
+// person.greeting();
+
+            // dynamic nature of objects
+
+// const person ={
+//     name:"Akash"
+// }
+// person.age=24;
+// person.greeting=function(){
+//     person.place="Vaniyambadi";
+// }
+
+//  console.log(person);
+
+            // Primitive value type
+
+// let cardObj={
+//     value:5
+// };
+// function updateCard(){
+//     cardObj.value++;
+// }
+// updateCard(cardObj)
+
+// console.log("Card avalible:",cardObj);
+
+
+            // Spread Operator
+
+// const user ={
+//     name:"Akash",
+//     getFullName(){
+//         console.log(`My name is ${this.name}`);
+//     }
+
+// }
+// // Copy the user object to another
+
+// let another ={...user.getFullName()};
+// console.log(another);
+
+
+                
+
+
+
 // Arrays
 //  Push() add the elements on end
 let names=['RDX','Akash'];
